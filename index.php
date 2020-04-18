9cc321b09b5d944d1a8edb867d8f5d4acb364e1e
Carlos Pedro Gomes <57367344+pedroiki@users.noreply.github.com>
2020-04-18 23:45:14 +0200

    basic website on php
    basic website on php

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+$title = "Home";
+$content = '
+        <img src="Images/actors2.png" class="imgLeft" />
+        <h3>Favourite Actors</h3>
+        <p>
+            Robert
+            or Tom
+        </p>
+
+        <img src="Images/rich.png" class="imgRight" />
+        <h3>Richest Actors</h3>
+        <p>
+          Dwayne Johnson
+         </p>
+
+         <img src="Images/director.png" class="imgLeft" />
+         <h3>Best Film director</h3>
+         <p>
+            Steven Spielberg
+         </p>';
+include 'Template.php';
+?>
